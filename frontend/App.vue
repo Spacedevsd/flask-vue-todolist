f806cb2ba0d146df9e53d1e73f4c60b333ed41f8
Marcus Pereira <mpereirassa@gmail.com>
2020-04-11 17:32:34 -0300

    format

--- frontend/App.vue
+++ frontend/App.vue
@@ -12,41 +12,41 @@
 </template>
 
 <script>
-import "bulma/css/bulma.css"
-import Todo from "./components/Todo"
+import "bulma/css/bulma.css";
+import Todo from "./components/Todo";
 
 export default {
-	name: "App",
-	components: {
-		"todo-component": Todo
-	}
+  name: "App",
+  components: {
+    "todo-component": Todo
+  }
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700&display=swap");
 
 .text-large {
-	font-size: 100px;
+  font-size: 100px;
 }
 
-html, body {
-	height: 100% !important;
+html,
+body {
+  height: 100% !important;
 }
 
 body {
-	font-family: 'Baloo Thambi 2', cursive;
+  font-family: "Baloo Thambi 2", cursive;
 }
 
 #form {
-	border: 5px solid rgb(43, 139, 102);
-	padding: 2rem;
-	height: 100% !important;
+  border: 5px solid rgb(43, 139, 102);
+  padding: 2rem;
+  height: 100% !important;
 }
 
 #form h1 {
-	margin-top: 2.5rem;
-	margin-bottom: 3.5rem;
+  margin-top: 2.5rem;
+  margin-bottom: 3.5rem;
 }
-
 </style>
--- frontend/components/Todo.vue
+++ frontend/components/Todo.vue
@@ -21,79 +21,78 @@
 
 <script>
 import Axios from "axios";
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 const http = Axios.create({
-	baseURL: "/ws/v1/todos"
-})
+  baseURL: "/ws/v1/todos"
+});
 
 export default {
-	name: "Todo",
-	data() {
-		return {
-			name: ""
-		}
-	},
-	async mounted() {
-		await this.getTodos()
-	},
-	computed: {
-		...mapState(["todoList"])
-	},
-	methods: {
-		...mapActions(["getTodos", "addTodo", "doneTodo"]),
-		
-		add() {
-			this.addTodo({
-				name: this.name,
-				status: true
-			})
-			this.name = ""
-		},
-		done(id) {
-			this.doneTodo(id)
-		}
-	}
-}
+  name: "Todo",
+  data() {
+    return {
+      name: ""
+    };
+  },
+  async mounted() {
+    await this.getTodos();
+  },
+  computed: {
+    ...mapState(["todoList"])
+  },
+  methods: {
+    ...mapActions(["getTodos", "addTodo", "doneTodo"]),
+
+    add() {
+      this.addTodo({
+        name: this.name,
+        status: true
+      });
+      this.name = "";
+    },
+    done(id) {
+      this.doneTodo(id);
+    }
+  }
+};
 </script>
 
 <style>
 .todo-list {
-	margin-top: 3rem;
+  margin-top: 3rem;
 }
 
 .todo-list li {
-	padding-top: 1rem;
-	padding-bottom: 1rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
 }
 
 .todo-list li span {
-	color: white;
+  color: white;
 }
 
 .todo-list li button ~ span {
-	margin-left: 16px;
+  margin-left: 16px;
 }
 
 .todo-list > li::before,
 .todo-list > li::after {
-	content: "";
-	display: table;
+  content: "";
+  display: table;
 }
 
 .todo-list > li::after {
-	clear: both;
+  clear: both;
 }
 
-.todo-list-divider > li:nth-child(n+2) {
-	margin-top: 10px;
-	margin-bottom: 10px;
-	padding-top: 2rem;
-	border-top: 1px solid #e5e5e5;
+.todo-list-divider > li:nth-child(n + 2) {
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding-top: 2rem;
+  border-top: 1px solid #e5e5e5;
 }
 
 .is-justified-between {
-	justify-content: space-between;
+  justify-content: space-between;
 }
-
 </style>
